8e84b7f8503ec419f50a8fbb7af0951f14dc70fd
Vitaliy <glacenkovitalij928@gmail.com>
2025-09-23 19:41:27 +0300

    the first file works well

---
+++ lab-1/hoisting.js
@@ -0,0 +1,16 @@
+// hoisting functions and variables
+
+// output is undefined
+console.log(myName); 
+
+// output is 'Vitaliy
+var myName = 'Vitaliy'
+console.log(myName);
+
+showName(myName);
+
+function showName(name) {
+    console.log('My name is ' + name);
+}
+
+
