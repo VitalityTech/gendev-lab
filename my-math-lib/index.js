25566deaec3a5a28356e9fa94d8e867388aa9878
Vitaliy <glacenkovitalij928@gmail.com>
2026-03-13 13:18:25 +0200

    Refine demo output and package metadata formatting

--- my-math-lib/index.js
+++ my-math-lib/index.js
@@ -1,14 +1,19 @@
-const { infiniteGenerator } = require('./infiniteGenerator');
-const { timeoutIterator, createNumberStatsProcessor } = require('./timeoutIterator');
-const { incrementalCounterGenerator } = require('./generators/incrementalCounterGenerator');
-const { fibonacciGenerator } = require('./generators/fibonacciGenerator');
-const { roundRobinGenerator } = require('./generators/roundRobinGenerator');
+const { infiniteGenerator } = require("./infiniteGenerator");
+const {
+  timeoutIterator,
+  createNumberStatsProcessor,
+} = require("./timeoutIterator");
+const {
+  incrementalCounterGenerator,
+} = require("./generators/incrementalCounterGenerator");
+const { fibonacciGenerator } = require("./generators/fibonacciGenerator");
+const { roundRobinGenerator } = require("./generators/roundRobinGenerator");
 
 module.exports = {
-	infiniteGenerator,
-	timeoutIterator,
-	createNumberStatsProcessor,
-	incrementalCounterGenerator,
-	fibonacciGenerator,
-	roundRobinGenerator
+  infiniteGenerator,
+  timeoutIterator,
+  createNumberStatsProcessor,
+  incrementalCounterGenerator,
+  fibonacciGenerator,
+  roundRobinGenerator,
 };
